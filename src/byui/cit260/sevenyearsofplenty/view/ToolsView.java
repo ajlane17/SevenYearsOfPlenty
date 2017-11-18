9d57b13cf3d6a60b80867d1b19b4c9c6995ea171
vmarcos <vmarcos@K011702188.esan.edu.pe>
2017-11-18 12:54:47 -0500

    Add the ToolsView with Array.

--- src/byui/cit260/sevenyearsofplenty/view/ToolsView.java
+++ src/byui/cit260/sevenyearsofplenty/view/ToolsView.java
@@ -11,17 +11,20 @@ package byui.cit260.sevenyearsofplenty.view;
  */
 public class ToolsView {
      public static void displayTools () {
+         String[] tools = {"1 - Shovel","2 - Hoe","3 - Rake","4 - Pruning shears",
+        "5 - Almocafre","6 - Wheelbarrow","7 - Watering can"};
+         
          
         System.out.println("##############################\n"
                          + "# WELCOME TO TOOL MAKER SHOP #\n"
                          + "##############################\n");
-        System.out.format("1 - Shovel\n");
-        System.out.format("2 - Hoe\n");
-        System.out.format("3 - Rake\n");
-        System.out.format("4 - Pruning shears\n");
-        System.out.format("5 - Almocafre\n");
-        System.out.format("6 - Wheelbarrow\n");
-        System.out.format("7 - Watering can\n");
-    }
-
+        int counter = 0;
+        while(counter < tools.length){
+            System.out.println(tools[counter]);
+            counter++;
+        }
+         
+     }
 }
+
+
