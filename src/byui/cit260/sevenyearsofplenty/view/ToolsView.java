c93643f676377bd89ef1ae417619b23c19b18fd7
vmarcos <vmarcos@K011702188.esan.edu.pe>
2017-11-18 12:19:33 -0500

    New view for the tools list.

---
+++ src/byui/cit260/sevenyearsofplenty/view/ToolsView.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.sevenyearsofplenty.view;
+
+/**
+ *
+ * @author vmarcos
+ */
+public class ToolsView {
+     public static void displayTools () {
+         
+        System.out.println("##############################\n"
+                         + "# WELCOME TO TOOL MAKER SHOP #\n"
+                         + "##############################\n");
+        System.out.format("1 - Shovel\n");
+        System.out.format("2 - Hoe\n");
+        System.out.format("3 - Rake\n");
+        System.out.format("4 - Pruning shears\n");
+        System.out.format("5 - Almocafre\n");
+        System.out.format("6 - Wheelbarrow\n");
+        System.out.format("7 - Watering can\n");
+    }
+
+}
