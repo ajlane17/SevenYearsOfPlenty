86dfcb18207d1813caa540ea564ef52ed9ed6445
Vanessa <vanemb8@hotmail.com>
2017-12-03 22:13:45 -0500

    Print the tools inventory in a text file.

--- src/byui/cit260/sevenyearsofplenty/view/ToolsView.java
+++ src/byui/cit260/sevenyearsofplenty/view/ToolsView.java
@@ -5,7 +5,9 @@
  */
 package byui.cit260.sevenyearsofplenty.view;
 
+import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import sevenyearsofplenty.SevenYearsOfPlenty;
 
 /**
@@ -17,20 +19,38 @@ public class ToolsView {
     protected static final PrintWriter console = SevenYearsOfPlenty.getOutFile();
     
      public static void displayTools () {
+         
+         StringBuilder report = new StringBuilder();
+         
+        String rowFormat = "Tool: %-8s" ;
+        int counter = 0;
+              
          String[] tools = {"1 - Shovel","2 - Hoe","3 - Rake","4 - Pruning shears",
         "5 - Almocafre","6 - Wheelbarrow","7 - Watering can"};
          
-         
-        console.println("##############################\n"
+                         
+        String header = "##############################\n"
                          + "# WELCOME TO TOOL MAKER SHOP #\n"
-                         + "##############################\n");
-        int counter = 0;
+                         + "##############################\n" ;
+        report.append(header);            
+       
         while(counter < tools.length){
             console.println(tools[counter]);
             counter++;
+            saveToolsReport(report);
         }
-         
+        
+        report.append(tools);
      }
+     
+     private static void saveToolsReport(StringBuilder report) {
+        
+        try(PrintWriter toolsReport = new PrintWriter(new FileWriter("tools.txt"), true)) {
+            toolsReport.print(report);
+        } catch (Exception e) {
+            ErrorView.display("toolsView", e.getMessage());
+        }
+    }
 }
 
 
